8ca05454f7c94420717d4ebf7477490b753d30a7
Gayatri jha <gayatrixreal@gmail.com>
2025-12-06 18:01:20 +0530

    2

--- DSA Round 1.py
+++ DSA Round 1.py
@@ -80,3 +80,19 @@ def print_zeros_then_ones(arr):
 # Example usage:
 array = [0, 1, 0, 1, 1, 0, 0, 1]
 print_zeros_then_ones(array)
+
+arr = [1, 0, 1, 0, 0, 1]
+print("Output:", end=" ")
+
+# Print all the 0s
+for num in arr:
+    if num == 0:
+        print(0, end=" ")
+
+# Print all the 1s
+for num in arr:
+    if num == 1:
+        print(1, end=" ")
+
+
+# cause i was not getting the logic
